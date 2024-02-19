2794f40d249a1299106bcebbb67284442868517b
Ángel Borrás <angeloki1963@gmail.com>
2024-02-19 13:14:20 +0100

    Preguntandome como realizar el programa

--- GenerarHistograma.py
+++ GenerarHistograma.py
@@ -1,27 +1,11 @@
 """
-Primero vamos a crear un generador de numeros aleatorios
+
+1. ¿Que debemos mostrar? El histograma
+2. ¿De donde obtenemos los valores del histograma? De un generador de numeros pseudoaleatorios
+3. ¿Donde almacenaremos los resultados del generador? En una array
+4. ¿Como sabremos la cantidad de veces que toco cada resultado? Usando el count() o un def que compare numeros pendiente preguntar al albert
+5. ¿Cuantos valores debera tener el histograma? A eleccion del usuario, creamos variable de tiradas
+6. ¿Como sacamos los numeros pseudoaleatorios? Usando la cantidad de tiradas como semillas
+7. ¿Como  se mostraran los datos? Podemos usar prints de los posibles resultados que muestres pipes de la cantidad de estos mismos
+
 """
-
-Time = 0  # Inicializamos el contador de tiempo
-
-# Inicializamos el contador de tiempo
-Time = 0  
-
-# Guardamos el tiempo inicial del sistema en segundos
-start_time = sum(map(int, str(__import__('time').time()).split('.')))
-
-while True:
-    # Solicitamos la entrada del usuario
-    Tiradas = input("Agregue numero de tiradas: ")
-
-    # Si el usuario ha ingresado algo, salimos del bucle
-    if Tiradas:
-        break
-    else:
-        # Si el usuario no ha ingresado nada, actualizamos el tiempo actual del sistema y aumentamos el contador con el tiempo que tardó
-        end_time = sum(map(int, str(__import__('time').time()).split('.')))
-        Time += end_time - start_time
-        start_time = end_time
-
-print("El usuario ingresó:", Tiradas)
-print("El tiempo que tardó el usuario en ingresar la variable fue:", Time, "segundos")
